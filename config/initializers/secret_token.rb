a191ddc2e8410ab3af4f6a5645c0ca659ad381e5
Sean Hussey <sean@seanhussey.com>
2012-11-15 12:29:45 -0500

    Initial commit (based on rails new -T -O --skip-bundle)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTemplate::Application.config.secret_token = '6561ef39faa65d8859f2c4fcbf34024dd4026e77848cd1588d5063c11419471e61b9a2c2006eb788dd91688fab3efa9209ac9aeca1b6bb020318279a2c45d7ec'
